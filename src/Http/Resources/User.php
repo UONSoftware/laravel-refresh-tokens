8f45763f9ce2eb0ddd9f6e9677d766c4d9f9ecc1
Dusan Malusev <dusan.998@outlook.com>
2019-12-07 14:56:32 +0100

    Adding user resource

--- src/Http/Resources/User.php
+++ src/Http/Resources/User.php
@@ -4,9 +4,23 @@
 namespace UonSoftware\RefreshTokens\Http\Resources;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class User extends JsonResource
 {
-
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'name'  => $this->resource->name,
+            'email' => $this->resource->email,
+        ];
+    }
 }
